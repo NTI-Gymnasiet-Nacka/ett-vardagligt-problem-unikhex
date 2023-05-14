f0c04fd65cf6558d7517ddb122da004139918547
Arata <shahzod20030905@gmail.com>
2023-05-14 21:48:08 +0200

    fixed the issue with email not being sent

--- new_test_prototype/server.js
+++ new_test_prototype/server.js
@@ -15,7 +15,7 @@ console.log(now.format());
 dotenv.config();
 const API_KEY = process.env.API_KEY
 const PASSWORD = process.env.PASSWORD
-const EMAIL = process.env.EMAIL  
+const EMAIL = process.env.EMAIL
 
 const PORT = process.env.PORT || 3000;
 const app = express()
@@ -71,104 +71,112 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-// schedule cron jobs to send notifications
-// breakfast at 7:00
-cron.schedule('0 7 * * *', () => {
-  console.log(`Scheduled breakfast at ${moment().tz('Europe/Stockholm').format('YYYY-MM-DD HH:mm:ss')}`);
-  sendNotifications('Breakfast');
-});
-
-// lunch at 12:00
-cron.schedule('0 12 * * *', () => {
-  console.log(`Scheduled lunch at ${moment().tz('Europe/Stockholm').format('YYYY-MM-DD HH:mm:ss')}`);
-  sendNotifications('Lunch');
-});
-
-// supper at 18:30
-cron.schedule('30 18 * * *', () => {
-  console.log(`Scheduled supper at ${moment().tz('Europe/Stockholm').format('YYYY-MM-DD HH:mm:ss')}`);
-  sendNotifications('Supper');
-});
-
-// snacktime at 17:00
-cron.schedule('0 17 * * *', () => {
-  console.log(`Scheduled snacktime at ${moment().tz('Europe/Stockholm').format('YYYY-MM-DD HH:mm:ss')}`);
-  sendNotifications('Snacktime');
-});
 
 // function to send notifications to users
-function sendNotifications(meal) {
-  db.all('SELECT * FROM users', (err, rows) => {
-    if (err) {
-      console.error(err.message);
-    } else {
-      rows.forEach((user) => {
-        // send notification to user
-        console.log(`Sending notification to ${user.email} for ${meal}`);
+function sendNotifications() {
+  return new Promise((resolve, reject) => {
+    db.all('SELECT * FROM users', (err, rows) => {
+      if (err) {
+        console.error(err.message);
+        reject(err);
+      } else {
+        rows.forEach(async (user) => {
+          try {
+            // send notification to user
+            const meal = await  getRecipes();
+            await sleep(2000);
+            // console.log(`Sending notification to ${user.email} for ${JSON.stringify(meal)}`);
 
-        // create transporter object with SMTP settings
-        const transporter = nodemailer.createTransport({
-          host: process.env.SMTP_HOST,
-          port: process.env.SMTP_PORT,
-          secure: false, // upgrade later with STARTTLS
-          auth: {
-            user: EMAIL,
-            pass: PASSWORD
+            // create transporter object with SMTP settings
+            const transporter = nodemailer.createTransport({
+              host: process.env.SMTP_HOST,
+              port: process.env.SMTP_PORT,
+              secure: false, // upgrade later with STARTTLS
+              auth: {
+                user: EMAIL,
+                pass: PASSWORD
+              }
+            });
+
+            // define email message
+            const message = {
+              from: process.env.EMAIL,
+              to: user.email,
+              // subject: `Your Daily ${meal}`,
+              text: `From the best here to remind you to eat something for ${JSON.stringify(meal)}`
+            };
+            console.log(message);
+
+            // send mail with defined transport object
+            transporter.sendMail(message, (err, info) => {
+              if (err) {
+                console.log(`Error sending email to ${user.email}: ${err}`);
+              } else {
+                console.log(`Email sent to ${user.email}: ${info.response}`);
+              }
+            });
+          } catch (error) {
+            console.error(error);
+            reject(error);
           }
         });
-
-        // define email message
-        const message = {
-          from: process.env.EMAIL,
-          to: user.email,
-          subject: `Your Daily ${meal}`,
-          text: `From the best here to remind you to eat something for ${meal}`
-        };
-         
-        // send mail with defined transport object
-        transporter.sendMail(message, (err, info) => {
-          if (err) {
-            console.log(`Error sending email to ${user.email}: ${err}`);
-          } else {
-            console.log(`Email sent to ${user.email}: ${info.response}`);
-          }
-        }); 
-      });
-    }
+        resolve();
+      }
+    });
   });
 }
 
+
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
 // route to get recipe based on ingredients
-app.get('/api/recipe/:ingredients', async (req, res) => {
-  const ingredients = req.params.ingredients.split(',');
+async function getRecipes(){
   try {
-    const response = await 
-    axios.get('https://api.spoonacular.com/recipes/findByIngredients', {
-      params: {
-        ingredients: ingredients.join(','),
-        number: 5,
-        apiKey: API_KEY
-      }
-    });
-    const recipeId = response.data[0].id;
-    const recipeResponse = await axios.get(`https://api.spoonacular.com/recipes/${recipeId}/information`, {
-      params: {
-        apiKey: API_KEY
-      }
-    });
-    const recipe  = {
-      title: recipeResponse.data.title,
-      instructions: recipeResponse.data.instructions
-    };
-    res.json (recipe)
+    let recipes = await axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${API_KEY}&number=${1}`)
+    recipes = Object.values(recipes.data)
+    console.log('this')
+    sleep(3000)
+    return recipes
+    // recipes[0].forEach(recipe => {
+    //   console.log(recipe)§
+    // } )
+
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error retrieving recipe');
   }
-});
+};
 
 // serve index.html at the root URL
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-``
+// schedule cron jobs to send notifications
+// breakfast at 7:00
+// cron.schedule('0 7 * * *', () => {
+//   console.log(`Scheduled breakfast at ${moment().tz('Europe/Stockholm').format('YYYY-MM-DD HH:mm:ss')}`);
+//   sendNotifications('Breakfast');
+// });
+
+// // lunch at 12:00
+// cron.schedule('28` 13 * * *', () => {
+//   console.log(`Scheduled lunch at ${moment().tz('Europe/Stockholm').format('YYYY-MM-DD HH:mm:ss')}`);
+//   sendNotifications();
+// });
+
+// // supper at 18:30
+// cron.schedule('30 18 * * *', () => {
+//   console.log(`Scheduled supper at ${moment().tz('Europe/Stockholm').format('YYYY-MM-DD HH:mm:ss')}`);
+//   sendNotifications('Supper');
+// });
+
+// // snacktime at 17:00
+// cron.schedule('0 17 * * *', () => {
+//   console.log(`Scheduled snacktime at ${moment().tz('Europe/Stockholm').format('YYYY-MM-DD HH:mm:ss')}`);
+//   sendNotifications('Snacktime');
+// });
+
+
+sendNotifications()
